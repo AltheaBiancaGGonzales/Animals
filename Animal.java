871026092d31f014771212ad06836c5f258cbbe0
Althea Gonzales <altheagonzalesctr@gmail.com>
2024-12-14 21:06:16 +0800

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,16 @@
+// Base class
+public abstract class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String makeSound();
+
+    public abstract String move();
+}
